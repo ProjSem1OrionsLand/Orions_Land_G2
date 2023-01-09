9a6984e462ef0b2564db821470eaab6d3fa04b11
AlanZ97 <76967170+AlanZ971@users.noreply.github.com>
2023-01-10 00:45:34 +0100

    Code respawn props

---
+++ OrionsLand_PrjSm1_G2/Assets/Préfabs/Scripts_Projet/PropsRespawn.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsRespawn : MonoBehaviour
+{
+	[Header("Liens Scripts")]
+	public SpellDetection spellValid;
+
+	[Space(10)]
+
+	[Header("Info Spawn GameObject")]
+	public GameObject propsToSpawn;
+	public Transform spawnPoint;
+
+	[Space(10)]
+
+	[Tooltip("Temps pour respawn le GameObject")]
+	public float respawnTime = 10f;
+	public float currentRespawnTime;
+
+	void Start()
+	{
+		currentRespawnTime = respawnTime;
+	}
+
+	void FixedUpdate()
+	{
+        if (spellValid.shoot == true)
+        {
+			currentRespawnTime -= 1*Time.deltaTime;
+			if(currentRespawnTime <= 0f)
+            {
+				GameObject props = Instantiate(propsToSpawn, spawnPoint.position, spawnPoint.rotation);
+				spellValid.shoot = false;
+				currentRespawnTime = respawnTime;
+			}
+		}
+
+	}
+}
--- OrionsLand_PrjSm1_G2/Assets/Préfabs/Scripts_Projet/SpellDetection.cs
+++ OrionsLand_PrjSm1_G2/Assets/Préfabs/Scripts_Projet/SpellDetection.cs
@@ -4,40 +4,54 @@ using UnityEngine;
 
 public class SpellDetection : MonoBehaviour
 {
-    [Header("PositionGameObject")]
+    [Header("Position GameObject")]
     public Transform propPosition;
+    public Transform respawnPosition;
 
-    [Space (10)]
+    [Space(10)]
     [Header("Particle")]
-
     public GameObject particleFire;
     public GameObject particleWater;
     public GameObject particleWind;
 
-    void Start()
+    [Space(10)]
+    [Header("Les Timeurs")]
+    public float lifetimeRemaining = 5f;
+
+
+    [Space(10)]
+    [Header("Variables")]
+    public bool shoot;
+    void Awake()
     {
-        
+        shoot = false;
+
     }
+
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "SpellFeu")
         {
             GameObject particle = Instantiate(particleFire, propPosition.position, propPosition.rotation);
+            Destroy(gameObject, lifetimeRemaining);
+            shoot = true;
         }
 
         else if (collision.gameObject.tag == "SpellEau")
         {
             GameObject particle = Instantiate(particleWater, propPosition.position, propPosition.rotation);
+            Destroy(gameObject, lifetimeRemaining);
+            shoot = true;
+
         }
 
         else if (collision.gameObject.tag == "SpellVent")
         {
             GameObject particle = Instantiate(particleWind, propPosition.position, propPosition.rotation);
+            Destroy(gameObject, lifetimeRemaining);
+            shoot = true;
+
         }
-    }
-    void Update()
-    {
-        
     }
 }
